bdb6a0035b34f915d4d6a81c7984433f296d27a6
uha-g.vuillemin <gauthier.vuillemin@uha.fr>
2019-09-23 16:16:49 +0200

    TODO: finish test in boardTest

--- src/test/ChessGameTest/Model/Test/TestBoard.java
+++ src/test/ChessGameTest/Model/Test/TestBoard.java
@@ -9,6 +9,7 @@ import ChessGame.Share.Enum.EElement;
 import ChessGame.Share.Iterator.BoardIterator;
 import ChessGameTest.Model.ClassTest.BoardClassTest;
 import junit.framework.TestCase;
+import org.junit.Test;
 
 
 
@@ -29,8 +30,6 @@ public class TestBoard extends TestCase {
     public void testGenerateBoard() {
         int count = 0, size = CBoard.TILE_HEIGHT_TAB * CBoard.TILE_WIDTH_TAB;
 
-        this.boardClassTest = new BoardClassTest();
-        this.boardClassTest.generateBoard();
         BoardIterator bi = this.boardClassTest.iterator();
 
         while (bi.hasNext()) {
@@ -42,13 +41,12 @@ public class TestBoard extends TestCase {
         assertEquals("generation board", size, count);
     }
 
+
     public void testGetElement() {
         boolean isEmptyElement = false, isPawnElement = false;
         boolean validate = false;
 
-        BoardClassTest boardClassTest = new BoardClassTest();
-        boardClassTest.generateBoard();
-        boardClassTest.initPiecesPosition(EColorChess.WHITE);
+        this.boardClassTest.initPiecesPosition(EColorChess.WHITE);
 
         if (boardClassTest.getElement(0, 0).getElement() == EElement.EMPTY) {
             isEmptyElement = true;
@@ -65,15 +63,14 @@ public class TestBoard extends TestCase {
         assertTrue("testGetElement", validate);
     }
 
+
     public void testInitPiecesPosition() {
         int countWhitePawn = 0, whitePawnNb = 8;
 
-        BoardClassTest boardClassTest = new BoardClassTest();
-        boardClassTest.generateBoard();
-        boardClassTest.initPiecesPosition(EColorChess.WHITE);
+        this.boardClassTest.initPiecesPosition(EColorChess.WHITE);
 
-        for(int x = 0; x < boardClassTest.getWidth(); x++) {
-            if (boardClassTest.getElement(6, x) instanceof Pawn) {
+        for(int x = 0; x < this.boardClassTest.getWidth(); x++) {
+            if (this.boardClassTest.getElement(6, x) instanceof Pawn) {
                 countWhitePawn++;
             }
         }
@@ -81,17 +78,32 @@ public class TestBoard extends TestCase {
         assertEquals("testInitPiecesPosition count number of pawn", whitePawnNb, countWhitePawn);
     }
 
+
+    //TODO: finish test
     public void testSetEmpty() {
         boolean isEmpty = false;
+        int posX = 0, posY = 6;
+        this.boardClassTest.initPiecesPosition(EColorChess.WHITE);
 
-        BoardClassTest boardClassTest = new BoardClassTest();
-        boardClassTest.generateBoard();
-        boardClassTest.initPiecesPosition(EColorChess.WHITE);
+        System.out.println(this.boardClassTest.getElement(posY, posX).getElement());
 
+        BoardElement boardElement = this.boardClassTest.getElement(posY, posX);
 
+        if(boardElement.getElement().toString() == EElement.) {
+            System.out.println("setEmpty");
+            this.boardClassTest.setEmptyElement(posY, posX);
+        }
 
+        boardElement = this.boardClassTest.getElement(posY, posX);
+        if (boardElement.getElement() == EElement.EMPTY) {
+            System.out.println("setTrue");
+            isEmpty = true;
+        }
+
+        assertTrue("testSetEmpty", isEmpty);
     }
 
+    //TODO: finish test
     public void testMovePiece() {
         boolean isEmptyElement = false, isPawnElement = false;
         boolean isPieceMoved = false;
@@ -103,9 +115,17 @@ public class TestBoard extends TestCase {
         boardClassTest.initPiecesPosition(EColorChess.WHITE);
 
         BoardElement boardElement = boardClassTest.getElement(initialPosY, initialPosX);
-        if (boardClassTest.isInstanceOfPiece(boardElement)) {
-            Piece piece = (Piece) boardElement;
-            boardClassTest.moveElement(movedPosY, movedPosX, piece);
+
+        if (boardClassTest.getElement(initialPosY, initialPosX).getElement() == EElement.EMPTY) {
+            isEmptyElement = true;
+        }
+
+        if (boardClassTest.getElement(movedPosY, movedPosX).getElement() == EElement.PAWN) {
+            isPawnElement = true;
+        }
+
+        if (isEmptyElement && isPawnElement) {
+            isPieceMoved = true;
         }
 
 
