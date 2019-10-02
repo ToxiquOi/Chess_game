e6ee2f7097a6c9e6d85f17ac0945223988bae8f8
name <email>
2019-10-02 09:24:32 +0200

    update sonarcloud

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class MainTest {
+    @Test
+    public void test(){
+        assertNotNull(1, "test");
+    }
+}
+
