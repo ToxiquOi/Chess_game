876961891712ff970173979f62ceb643563a71de
name <email>
2019-10-02 13:33:31 +0200

    fix minor bug

--- src/main/java/org/chessgame/view/awt/graphics/CaseBoard.java
+++ src/main/java/org/chessgame/view/awt/graphics/CaseBoard.java
@@ -5,14 +5,14 @@ import java.awt.*;
 
 public class CaseBoard {
 
-    private static int Width = 100;
-    private static int Height = 100;
+    private static int width = 100;
+    private static int height = 100;
 
     public CaseBoard(int posX, int posY, Graphics g){
-        g.fillRect(posX * 100, posY * 100, Width, Height);
+        g.fillRect(posX * 100, posY * 100, width, height);
     }
 
     public static void draw(int posX, int posY, Graphics g) {
-        g.fillRect(posX * 100, posY * 100, Width, Height);
+        g.fillRect(posX * 100, posY * 100, width, height);
     }
 }
